434be555c0608c54aa05a2ef03a097174be05dff
Ike <ikescode@gmail.com>
2020-09-21 19:22:06 -0500

    add contenthash to output bundle

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, '/dist'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: "bundle.[contenthash].js"
   },
   devServer: {
     contentBase: './dist',
