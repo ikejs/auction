716fc2a1c8e5f7d8b86d861b34c2bf26334c029b
Ike <ikescode@gmail.com>
2020-09-23 13:57:33 -0500

    change phone to string

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose  = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true},
-  phone: Number
+  phone: String
 }, {
   timestamps: true
 });
